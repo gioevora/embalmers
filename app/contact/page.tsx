00e4490f3a343edaf52f3157a1eb8b15a21da9b5
unknown <giolo.evora@gmail.com>
2025-01-20 16:49:51 +0800

    update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -35,7 +35,7 @@ const ContactSection = () => {
                 height="450"
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3861.686618330025!2d121.01093307574082!3d14.559904978070435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c90b830e5f29%3A0x89fe307dfecd3c0d!2sCampos%20Rueda%20Building!5e0!3m2!1sen!2sph!4v1734658577068!5m2!1sen!2sph"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7811.110573346462!2d121.15413722909844!3d13.388872876411778!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bceed6e29309c7%3A0x963e2dc010195599!2sMindoro%20State%20University%2C%20Calapan%20City%20Campus!5e1!3m2!1sen!2sph!4v1737362942478!5m2!1sen!2sph"
                 style={{ border: 0 }}
                 title="Google Maps Embed"
               />
@@ -56,7 +56,7 @@ const ContactSection = () => {
                           className="text-md md:text-lg"
                           href="tel:09651983796"
                         >
-                          09xxxxx
+                          09065524625
                         </Link>
                       </p>
                     </div>
--- app/video/page.tsx
+++ app/video/page.tsx
@@ -1,15 +1,12 @@
 export default function VideoPage() {
   return (
-    <div>
-      <div>
-        <iframe
-          allow="autoplay"
-          height="480"
-          src="https://drive.google.com/file/d/1k9JJuNZ4QoNWwu-nF-nnJsq_2N64IzpM/preview"
-          title="Video preview from Google Drive" // Added title for accessibility
-          width="1000"
-        />
-      </div>
+    <div style={{ position: "relative", paddingBottom: "56.25%", height: 0, overflow: "hidden", maxWidth: "100%", width: "100%" }}>
+      <iframe
+        allow="autoplay"
+        src="https://drive.google.com/file/d/1k9JJuNZ4QoNWwu-nF-nnJsq_2N64IzpM/preview"
+        title="Video preview from Google Drive"
+        style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }}
+      />
     </div>
   );
 }
